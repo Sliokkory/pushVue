3cf12cd7414b89efbe62fcb5349957c6507ce9d6
Ilya <50696983+Sliokkory@users.noreply.github.com>
2024-04-05 02:51:04 +0300

    push done

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,40 +1,18 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('../firebase-messaging-sw.js')
-      .then(function(registration) {
-        console.log('Registration successful, scope is:', registration.scope);
-      }).catch(function(err) {
-        console.log('Service worker registration failed, error:', err);
-      });
-    }
+importScripts('https://www.gstatic.com/firebasejs/8.2.1/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/8.2.1/firebase-messaging.js')
 
-// Scripts for firebase and firebase messaging
-importScripts('https://www.gstatic.com/firebasejs/<v10+>/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/<v10+>/firebase-messaging.js');
+console.log('===========================================')
+console.log('FIREBASE SERVICE WORKER LOADED')
+console.log('===========================================')
 
+firebase.initializeApp({
+  apiKey: 'AIzaSyBCwKyqnxpHAdezdI-bQVQkP6PrSUrFNoA',
+  authDomain: 'pushvue-ca59f.firebaseapp.com',
+  projectId: 'pushvue-ca59f',
+  storageBucket: 'pushvue-ca59f.appspot.com',
+  messagingSenderId: '945047635984',
+  appId: '1:945047635984:web:2478af7c035d9327522295',
+  measurementId: 'G-WSDRNKP2S5'
+})
 
-// Initialize the Firebase app in the service worker by passing the generated config
-const firebaseConfig = {
-    apiKey: "AIzaSyBCwKyqnxpHAdezdI-bQVQkP6PrSUrFNoA",
-    authDomain: "pushvue-ca59f.firebaseapp.com",
-    projectId: "pushvue-ca59f",
-    storageBucket: "pushvue-ca59f.appspot.com",
-    messagingSenderId: "945047635984",
-    appId: "1:945047635984:web:2478af7c035d9327522295",
-    measurementId: "G-WSDRNKP2S5"
-  };
- 
-  firebase.initializeApp(firebaseConfig);
- 
-  // Retrieve firebase messaging
-  const messaging = firebase.messaging();
- 
-  messaging.onBackgroundMessage(function(payload) {
-    console.log("Received background message ", payload);
- 
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-      body: payload.notification.body,
-    };
- 
-    self.registration.showNotification(notificationTitle, notificationOptions);
-  });
+const messaging = firebase.messaging()
--- src/App.vue
+++ src/App.vue
@@ -2,58 +2,53 @@
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { initializeApp } from 'firebase/app'
+import { getMessaging, getToken, onMessage } from 'firebase/messaging'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBCwKyqnxpHAdezdI-bQVQkP6PrSUrFNoA",
-  authDomain: "pushvue-ca59f.firebaseapp.com",
-  projectId: "pushvue-ca59f",
-  storageBucket: "pushvue-ca59f.appspot.com",
-  messagingSenderId: "945047635984",
-  appId: "1:945047635984:web:2478af7c035d9327522295",
-  measurementId: "G-WSDRNKP2S5"
-};
+  apiKey: 'AIzaSyBCwKyqnxpHAdezdI-bQVQkP6PrSUrFNoA',
+  authDomain: 'pushvue-ca59f.firebaseapp.com',
+  projectId: 'pushvue-ca59f',
+  storageBucket: 'pushvue-ca59f.appspot.com',
+  messagingSenderId: '945047635984',
+  appId: '1:945047635984:web:2478af7c035d9327522295',
+  measurementId: 'G-WSDRNKP2S5'
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig)
 const messaging = getMessaging(app)
 
-Notification.requestPermission()
-    .then(permission => {
-      if (permission === "granted") {
-        getToken(messaging, {
-          vapidKey:
-          firebaseConfig.apiKey,
-        })
-          .then((currentToken) => {
-            if (currentToken) {
-              console.log("Firebase Token", currentToken);
-            } else {
-              // Show permission request UI
-              console.log(
-                "No registration token available. Request permission to generate one."
-              );
-              // ...
-            }
-          })
-          .catch((err) => {
-            console.log("An error occurred while retrieving token. ", err);
-            // ...
-          });
-
-        onMessage(messaging, (payload) => {
-          console.log("Message received. ", payload);
+Notification.requestPermission().then((permission) => {
+  if (permission === 'granted') {
+    getToken(messaging, {
+      vapidKey:
+        'BNKN0pwkjFZEh4xfPWH8_tmmEFzeJYo6bFcE8peRGp3duEr0c8eFDuU4kMAo8asd15Z_arIBej6oS9UJoAcObF4'
+    })
+      .then((currentToken) => {
+        if (currentToken) {
+          console.log('Firebase Token', currentToken)
+        } else {
+          // Show permission request UI
+          console.log('No registration token available. Request permission to generate one.')
           // ...
-        });
-      } else {
-        console.log("Unable to get permission to notify.");
-      }
-    });
+        }
+      })
+      .catch((err) => {
+        console.log('An error occurred while retrieving token. ', err)
+      })
+  } else {
+    console.log('Unable to get permission to notify.')
+  }
+})
+onMessage(messaging, (payload) => {
+  console.log('Message received. ', payload)
+  // ...
+})
 </script>
 
 <template>
@@ -98,3 +93,5 @@ header {
   }
 }
 </style>
+
+<!-- cd C:\Program Files\Google\Chrome\Application\ -> chrome.exe --ignore-certificate-errors --unsafely-treat-insecure-origin-as-secure=https://localhost:5173 --user-data-dir=C:\tmp -->
